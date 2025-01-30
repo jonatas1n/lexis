8d3ea9697e5b035201004c405481148227fde01e
jonatas1n <jonatasgeek@gmail.com>
2025-01-30 00:32:18 -0300

    chore: set bills path prefix

--- api/entities/bills/routes.py
+++ api/entities/bills/routes.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from .controllers import BillController
 
-router = APIRouter()
+router = APIRouter(prefix="/bills")
 
 router.get("/", response_model=list)(BillController.get_all_bills)
 router.get("/{bill_id}", response_model=dict)(BillController.get_bill)

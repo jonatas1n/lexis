cf3dff809dab24c69da1c33281afb73016942219
jonatas1n <jonatasgeek@gmail.com>
2025-01-29 21:43:37 -0300

    refact: move message to constant

--- api/entities/bills/controllers.py
+++ api/entities/bills/controllers.py
@@ -1,7 +1,7 @@
 from fastapi import HTTPException
 from .services import BillServices
 
-
+NOT_FOUND_MESSAGE = "Bill not found"
 class BillController:
     @staticmethod
     def get_all_bills():
@@ -11,5 +11,5 @@ class BillController:
     def get_bill(bill_id):
         bill = BillServices.get_by_id(bill_id)
         if not bill:
-            raise HTTPException(status_code=404, detail="User not found")
+            raise HTTPException(status_code=404, detail=NOT_FOUND_MESSAGE)
         return bill

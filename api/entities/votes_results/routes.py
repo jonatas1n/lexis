43ca99e3996c20c5e3850b1cec1488bd8c9ea7ce
jonatas1n <jonatasgeek@gmail.com>
2025-01-30 00:32:12 -0300

    chore: set votes_results path prefix

--- api/entities/votes_results/routes.py
+++ api/entities/votes_results/routes.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from .controllers import VotesResultController
 
-router = APIRouter()
+router = APIRouter(prefix="/votes_results")
 
 router.get("/", response_model=list)(VotesResultController.get_all_votes_results)
 router.get("/{vote_result_id}", response_model=dict)(

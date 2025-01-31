6912375690fa3115c2cc109c9aae50fdaa143312
jonatas1n <jonatasgeek@gmail.com>
2025-01-31 19:34:42 -0300

    refact: enhance bills services with an util

--- api/src/entities/bills/services.py
+++ api/src/entities/bills/services.py
@@ -5,6 +5,28 @@ from ..votes_results.repositories import VotesResultRepository
 from .repositories import BillRepository
 
 
+def process_bill(bill: dict, votes: list[dict], votes_results: list[dict]):
+    bill = {**bill, "opposed_votes": 0, "support_votes": 0}
+    bill_votes = [vote["id"] for vote in votes if vote["bill_id"] == bill["id"]]
+    if not bill_votes:
+        return bill
+
+    bill_votes_results = [
+        vote_result
+        for vote_result in votes_results
+        if vote_result["vote_id"] in bill_votes
+    ]
+    if not bill_votes_results:
+        return bill
+
+    for vote in bill_votes_results:
+        vote_type = (
+            "support_votes" if vote["vote_type"] == 1 else "opposed_votes"
+        )
+        bill[vote_type] += 1
+    return bill
+
+
 class BillServices:
     @staticmethod
     def get_all(
@@ -15,24 +37,7 @@ class BillServices:
         votes_results = VotesResultRepository.read_csv()
 
         for index, bill in enumerate(bills):
-            bills[index] = {**bill, "opposed_votes": 0, "support_votes": 0}
-            bill_votes = [vote["id"] for vote in votes if vote["bill_id"] == bill["id"]]
-            if not bill_votes:
-                continue
-
-            bill_votes_results = [
-                vote_result
-                for vote_result in votes_results
-                if vote_result["vote_id"] in bill_votes
-            ]
-            if not bill_votes_results:
-                continue
-
-            for vote in bill_votes_results:
-                vote_type = (
-                    "support_votes" if vote["vote_type"] == 1 else "opposed_votes"
-                )
-                bills[index][vote_type] += 1
+            bills[index] = process_bill(bill, votes, votes_results)
 
         def filter_bill(bill: dict):
             if title and title.lower() not in bill["title"].lower():
@@ -43,7 +48,11 @@ class BillServices:
         return bills
 
     @staticmethod
-    def get_by_id(bill_id):
+    def get_by_id(bill_id: str):
         bill_id = int(bill_id)
         bills = BillRepository.read_csv()
-        return next((bill for bill in bills if bill["id"] == bill_id), None)
+        votes = VoteRepository.read_csv()
+        votes_results = VotesResultRepository.read_csv()
+        bill = next((bill for bill in bills if bill["id"] == bill_id), None)
+        bill = process_bill(bill, votes, votes_results)
+        return bill

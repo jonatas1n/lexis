25722358cd909e64729c44aadfd44c7d2689ede8
jonatas1n <jonatasgeek@gmail.com>
2025-01-31 04:35:54 -0300

    feat: add cors treatment

--- api/src/api.py
+++ api/src/api.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .entities.bills import router as bills_router
 from .entities.legislators import router as legislators_router
@@ -6,6 +7,14 @@ from .entities.votes import router as votes_router
 from .entities.votes_results import router as votes_results_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Adjust this to restrict access
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(bills_router)
 app.include_router(legislators_router)

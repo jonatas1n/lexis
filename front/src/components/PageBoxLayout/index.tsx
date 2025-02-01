cac499e8a8a0863a24d923d3ee3b58405811da21
jonatas1n <jonatasgeek@gmail.com>
2025-02-01 17:44:53 -0300

    feat: add logo to page layout

--- front/src/components/PageBoxLayout/index.tsx
+++ front/src/components/PageBoxLayout/index.tsx
@@ -1,20 +1,22 @@
 import { Card, Grid, GridItem, Link, Text } from "@chakra-ui/react";
 import { PropsWithChildren } from "react";
 import { FaArrowLeft } from "react-icons/fa";
+import { Logo } from "../Logo";
 
 export const PageBoxLayout = ({ children }: PropsWithChildren) => {
   return (
     <Grid w="100vh">
       <Grid gap="1rem" templateRows="3rem calc(100vh - 6rem)">
         <GridItem alignContent="center">
-          <Link href="/">
-            <FaArrowLeft /> <Text fontVariant="all-small-caps">Back</Text>
-          </Link>
+          <Grid w="100%" templateColumns="1fr auto 1fr" alignItems="center">
+            <Link href="/" justifySelf="start">
+              <FaArrowLeft /> <Text fontVariant="all-small-caps">Back</Text>
+            </Link>
+            <Logo size={36} justifySelf="center" />
+          </Grid>
         </GridItem>
         <Card.Root>
-          <Card.Body h="100%">
-            {children}
-          </Card.Body>
+          <Card.Body h="100%">{children}</Card.Body>
         </Card.Root>
       </Grid>
     </Grid>

b38741c5741368fca09aab703e314b6541ed6fab
jonatas1n <jonatasgeek@gmail.com>
2025-02-01 14:04:02 -0300

    feat: use icon as avatar and change id display at legislator modal profile

--- front/src/pages/Legislators/LegislatorsModal/LegislatorProfile/index.tsx
+++ front/src/pages/Legislators/LegislatorsModal/LegislatorProfile/index.tsx
@@ -1,27 +1,25 @@
 import { Legislator } from "@/types";
-import { AvatarGroup, Avatar } from "@/components/ui/avatar";
-import {
-  Flex,
-  Grid,
-  Text,
-} from "@chakra-ui/react";
+import { Badge, Flex, Text, Avatar } from "@chakra-ui/react";
+import { FaUserTie } from "react-icons/fa";
 
 type LegislatorProfileProps = {
   legislator: Legislator;
-}
+};
 
-export const LegislatorProfile = ({legislator}: LegislatorProfileProps) => {
+export const LegislatorProfile = ({ legislator }: LegislatorProfileProps) => {
   return (
     <Flex gap={2} align="center">
-      <AvatarGroup>
-        <Avatar size="xl" />
-      </AvatarGroup>
-      <Grid>
+      <Avatar.Root size="xl">
+              <Avatar.Fallback>
+                <FaUserTie size={28} />
+              </Avatar.Fallback>
+            </Avatar.Root>
+      <Flex direction="column" align="flex-start">
         <Text fontWeight="700" fontSize={20}>
           {legislator.name}
         </Text>
-        <Text fontVariant="all-small-caps">#{legislator.id}</Text>
-      </Grid>
+        <Badge>#{legislator.id}</Badge>
+      </Flex>
     </Flex>
-  )
-}
+  );
+};

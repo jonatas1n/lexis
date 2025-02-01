014cf9cbbf56ec1c9653cc68eccc77000f7ab967
jonatas1n <jonatasgeek@gmail.com>
2025-02-01 12:52:39 -0300

    feat: implement progressBar to modals

---
+++ front/src/components/ProgressBar/index.tsx
@@ -0,0 +1,24 @@
+import { Progress, Flex, Text } from "@chakra-ui/react";
+
+type ProgressBarProps = {
+  value?: number | null;
+  total: number;
+};
+
+export const ProgresBar = ({ value = null, total }: ProgressBarProps) => {
+  return (
+    <Progress.Root value={value ?? null} max={total} size="lg">
+      <Progress.Track>
+        <Progress.Range />
+      </Progress.Track>
+      <Flex justify="space-between" mt={1}>
+        <Progress.Label>
+          {value && <Text>{value.toFixed(2)}% support votes</Text>}
+        </Progress.Label>
+        <Progress.Label>
+          {value && <Text>{(total - value).toFixed(2)}% oppose votes</Text>}
+        </Progress.Label>
+      </Flex>
+    </Progress.Root>
+  );
+};

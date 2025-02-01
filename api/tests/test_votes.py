99fa03234c12551038cfc3e2bfabaffcc0d19480
jonatas1n <jonatasgeek@gmail.com>
2025-02-01 20:01:13 -0300

    fix: remove no need files

--- api/tests/test_votes.py
+++ api/tests/test_votes.py
@@ -1,15 +1,7 @@
-from fastapi import HTTPException, FastAPI
 from unittest.mock import mock_open, patch
-from fastapi.testclient import TestClient
-from ..src.entities.votes import CSV_FILE, VoteRepository, VoteServices, VoteController
-from ..src.entities.votes.controllers import NOT_FOUND_MESSAGE
+from ..src.entities.votes import CSV_FILE, VoteRepository, VoteServices
 
 mock_csv_data = "id,bill_id\n1,2900994\n2,2900995\n"
-
-app = FastAPI()
-
-client = TestClient(app)
-
 
 def test_votes_repository_read_csv():
     expected_result = [
@@ -44,36 +36,3 @@ def test_votes_services_get_vote():
             result = VoteServices.get_by_id(vote_id)
             assert result == expected_result
 
-
-def test_votes_controller_get_all_votes():
-    expected_result = [
-        {"id": 1, "bill_id": 2900994},
-        {"id": 2, "bill_id": 2900995},
-    ]
-
-    with patch("builtins.open", mock_open(read_data=mock_csv_data)):
-        with patch("api.src.entities.votes.repositories.CSV_FILE", CSV_FILE):
-            result = VoteController.get_all_votes()
-            assert result == expected_result
-
-
-def test_votes_controller_get_vote():
-    expected_result = {"id": 1, "bill_id": 2900994}
-    vote_id = 1
-
-    with patch("builtins.open", mock_open(read_data=mock_csv_data)):
-        with patch("api.src.entities.votes.repositories.CSV_FILE", CSV_FILE):
-            result = VoteController.get_vote(vote_id)
-            assert result == expected_result
-
-
-def test_votes_controller_get_vote_empty():
-    vote_id = 3
-    expected_status_code = 404
-    with patch("builtins.open", mock_open(read_data=mock_csv_data)):
-        with patch("api.src.entities.votes.repositories.CSV_FILE", CSV_FILE):
-            try:
-                VoteController.get_vote(vote_id)
-            except HTTPException as e:
-                assert e.status_code == expected_status_code
-                assert e.detail == NOT_FOUND_MESSAGE
--- api/tests/test_votes_results.py
+++ api/tests/test_votes_results.py
@@ -1,20 +1,11 @@
-from fastapi import HTTPException, FastAPI
 from unittest.mock import mock_open, patch
-from fastapi.testclient import TestClient
 from ..src.entities.votes_results import (
     CSV_FILE,
     VotesResultRepository,
     VotesResultServices,
-    VotesResultController,
 )
-from ..src.entities.votes_results.controllers import NOT_FOUND_MESSAGE
 
 mock_csv_data = "id,legislator_id,vote_id,vote_type\n1,1,201,1\n2,2,202,2\n"
-
-app = FastAPI()
-
-client = TestClient(app)
-
 
 def test_votes_results_repository_read_csv():
     expected_result = [
@@ -48,37 +39,3 @@ def test_votes_results_services_get_votes_result():
         with patch("api.src.entities.votes_results.repositories.CSV_FILE", CSV_FILE):
             result = VotesResultServices.get_by_id(votes_result_id)
             assert result == expected_result
-
-
-def test_votes_results_controller_get_all_votes_results():
-    expected_result = [
-        {"id": 1, "legislator_id": 1, "vote_id": 201, "vote_type": 1},
-        {"id": 2, "legislator_id": 2, "vote_id": 202, "vote_type": 2},
-    ]
-
-    with patch("builtins.open", mock_open(read_data=mock_csv_data)):
-        with patch("api.src.entities.votes_results.repositories.CSV_FILE", CSV_FILE):
-            result = VotesResultController.get_all_votes_results()
-            assert result == expected_result
-
-
-def test_votes_results_controller_get_votes_result():
-    expected_result = {"id": 1, "legislator_id": 1, "vote_id": 201, "vote_type": 1}
-    votes_result_id = 1
-
-    with patch("builtins.open", mock_open(read_data=mock_csv_data)):
-        with patch("api.src.entities.votes_results.repositories.CSV_FILE", CSV_FILE):
-            result = VotesResultController.get_votes_result(votes_result_id)
-            assert result == expected_result
-
-
-def test_votes_results_controller_get_votes_result_empty():
-    votes_result_id = 3
-    expected_status_code = 404
-    with patch("builtins.open", mock_open(read_data=mock_csv_data)):
-        with patch("api.src.entities.votes_results.repositories.CSV_FILE", CSV_FILE):
-            try:
-                VotesResultController.get_votes_result(votes_result_id)
-            except HTTPException as e:
-                assert e.status_code == expected_status_code
-                assert e.detail == NOT_FOUND_MESSAGE

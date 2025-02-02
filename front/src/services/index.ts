92708ed5314bda0695d5455a1a5e55bda5c20c60
jonatas1n <jonatasgeek@gmail.com>
2025-02-02 17:54:27 -0300

    fix: update routes

--- front/src/services/index.ts
+++ front/src/services/index.ts
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+const FALLBACK_API_URL = 'http://localhost:8000'
+
+const API_URL = process.env.VITE_API_URL ?? FALLBACK_API_URL;
+
 export const apiClient = axios.create({
-  baseURL: "/api",
+  baseURL: API_URL,
   headers: {
     'Content-Type': 'application/json'
   }

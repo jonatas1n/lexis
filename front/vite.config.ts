58f94651d169406ff960f85153c5826251a17c3b
jonatas1n <jonatasgeek@gmail.com>
2025-02-02 18:17:07 -0300

    chore: finish route changing

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
-const FALLBACK_API_URL = 'http://localhost:8000'
 
-const API_URL = process.env.VITE_API_URL ?? FALLBACK_API_URL;
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
@@ -19,13 +17,6 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 3000,
-    strictPort: true,
-    proxy: {
-      '/api': {
-        target: API_URL,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
+    strictPort: true
   },
 });

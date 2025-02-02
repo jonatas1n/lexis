de420c6d15671dbe1977dd23565ddca14da94baa
jonatas1n <jonatasgeek@gmail.com>
2025-02-02 18:15:18 -0300

    chore: update path

--- front/src/services/index.ts
+++ front/src/services/index.ts
@@ -1,11 +1,7 @@
 import axios from 'axios';
 
-const FALLBACK_API_URL = 'http://localhost:8000'
-
-const API_URL = process.env.VITE_API_URL ?? FALLBACK_API_URL;
-
 export const apiClient = axios.create({
-  baseURL: API_URL,
+  baseURL: "/api",
   headers: {
     'Content-Type': 'application/json'
   }
--- front/vite.config.ts
+++ front/vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
+const FALLBACK_API_URL = 'http://localhost:8000'
+
+const API_URL = process.env.VITE_API_URL ?? FALLBACK_API_URL;
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
@@ -17,5 +20,12 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 3000,
     strictPort: true,
+    proxy: {
+      '/api': {
+        target: API_URL,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   },
 });

a64ec3566b4e1905e8b2ee68f3b8384d147bb639
jonatas1n <jonatasgeek@gmail.com>
2025-02-02 14:12:02 -0300

    chore: update VoteItem design

--- front/src/components/VoteItem/index.tsx
+++ front/src/components/VoteItem/index.tsx
@@ -1,26 +1,10 @@
-import { Grid, Text, Button } from "@chakra-ui/react";
 import { VotesResults } from "@/types";
+import { EntityItem } from "../EntityItem";
 
 type VoteResultItemType = {
   voteResult: VotesResults;
 };
 
 export const VoteResultItem = ({ voteResult }: VoteResultItemType) => {
-  return (
-    <Grid
-      as={Button}
-      color="black"
-      bgColor="#eee"
-      p={2}
-      borderRadius={8}
-      alignContent="center"
-      justifyContent="space-between"
-      templateColumns="auto 8rem"
-    >
-      <Grid gap={2} templateColumns="6rem auto">
-        <Text fontStyle="italic">#{voteResult.billId}</Text>
-        <Text>{voteResult.billTitle}</Text>
-      </Grid>
-    </Grid>
-  );
+  return <EntityItem title={voteResult.billTitle} />;
 };

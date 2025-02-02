091fec092ff4670a270508f1657a6f2c8c852836
jonatas1n <jonatasgeek@gmail.com>
2025-02-02 18:33:13 -0300

    fix: finish routes

--- front/src/services/index.ts
+++ front/src/services/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const FALLBACK_API_URL = 'http://localhost:8000'
 
-const API_URL = process.env.VITE_API_URL ?? FALLBACK_API_URL;
+const API_URL = import.meta.env.VITE_API_URL ?? FALLBACK_API_URL;
 
 export const apiClient = axios.create({
   baseURL: API_URL,
